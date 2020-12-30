81419227d4a3c86d4fb49576c21a783ebbb7b89e
Thomas McNish <mcnishthomas@gmail.com>
2020-12-29 20:32:02 -0500

    added title, header, logo, instructions, and styled them git status clear

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const doorImage1 = document.getElementById('door1');
 const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
-
+const robotDoorPath = "images/robot.svg";
 const beachDoorPath = "images/beach.svg";
 const spaceDoorPath = "images/space.svg";
 
 doorImage1.onclick = () => {
-    doorImage1.src = "images/robot.svg"
+    doorImage1.src = robotDoorPath;
 }
 
 doorImage2.onclick = () => {
