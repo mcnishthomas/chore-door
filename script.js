aa5d0e39c5af67da51a2616c3c9a02101ff1f308
Thomas McNish <mcnishthomas@gmail.com>
2020-12-29 20:04:27 -0500

    added click functionality to all 3 doors

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const doorImage1 = document.getElementById('door1');
+const doorImage2 = document.getElementById('door2');
+const doorImage3 = document.getElementById('door3');
+
+const beachDoorPath = "images/beach.svg";
+const spaceDoorPath = "images/space.svg";
 
 doorImage1.onclick = () => {
     doorImage1.src = "images/robot.svg"
+}
+
+doorImage2.onclick = () => {
+    doorImage2.src = beachDoorPath;
+}
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
 }
