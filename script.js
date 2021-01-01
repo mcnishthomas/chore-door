dd5a693d740ef4e4797d5d85e38a303a18e88429
Thomas McNish <mcnishthomas@gmail.com>
2021-01-01 14:30:23 -0500

    created isClicked and playDoor functions

--- script.js
+++ script.js
@@ -4,21 +4,46 @@ const doorImage3 = document.getElementById('door3');
 const botDoorPath = "images/robot.svg";
 const beachDoorPath = "images/beach.svg";
 const spaceDoorPath = "images/space.svg";
+const closedDoorPath = "images/closed_door.svg";
 let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
 
+isClicked = door => {
+    if (door.src === closedDoorPath) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+playDoor = () => {
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver();
+    }
+}
+
 doorImage1.onclick = () => {
-    doorImage1.src = openDoor1;
+    if (isClicked) {
+        doorImage1.src = openDoor1;
+        playDoor();
+    }
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = openDoor2;
+    if (isClicked) {
+        doorImage2.src = openDoor2;
+        playDoor();
+    }
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = openDoor3;
+    if (isClicked) {
+        doorImage3.src = openDoor3;
+        playDoor();
+    }
 }
 
 randomChoreDoorGenerator = () => {
